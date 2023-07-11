2db7816eebfce4a0d05acbb441e622a8731d779e
David Rivadeneyra <david.rivadeneyra.c@gmail.com>
2023-07-10 21:44:12 -0500

    new color palette

--- app/page.js
+++ app/page.js
@@ -2,14 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
 	return (
-		<div>
-			<h1>
+		<div className='p-5'>
+			<p className='text-gray-700'>
 				Mi nombre es David 🖖, soy Product Designer y profesor. Por más
 				de 4 años he estado creando productos que conecten con los
 				usuarios y ayuden a las empresas a alcanzar sus objetivos 🚀. Me
 				encanta la colaboración en equipo, las interfaces hermosas 🌈 y
 				las buenas prácticas.
-			</h1>
+			</p>
 		</div>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-	mode: 'jit',
+	// mode: 'jit',
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,6 +11,58 @@ module.exports = {
 		screens: {
 			tablet: '960px',
 			desktop: '1248px',
+		},
+		colors: {
+			gray: {
+				100: 'hsl(var(--gray-100), <alpha-value>)',
+				200: 'hsl(var(--gray-200), <alpha-value>)',
+				600: 'hsl(var(--gray-600), <alpha-value>)',
+				700: 'hsl(var(--gray-700), <alpha-value>)',
+				800: 'hsl(var(--gray-800), <alpha-value>)',
+				900: 'hsl(var(--gray-900), <alpha-value>)',
+			},
+			blue: {
+				100: 'hsl(var(--blue-100), <alpha-value>)',
+				200: 'hsl(var(--blue-200), <alpha-value>)',
+				500: 'hsl(var(--blue-500), <alpha-value>)',
+			},
+			orange: {
+				100: 'hsl(var(--orange-100), <alpha-value>)',
+				200: 'hsl(var(--orange-200), <alpha-value>)',
+				500: 'hsl(var(--orange-500), <alpha-value>)',
+			},
+			green: {
+				100: 'hsl(var(--green-100), <alpha-value>)',
+				200: 'hsl(var(--green-200), <alpha-value>)',
+				600: 'hsl(var(--green-500), <alpha-value>)',
+				700: 'hsl(var(--green-700), <alpha-value>)',
+				800: 'hsl(var(--green-800), <alpha-value>)',
+				900: 'hsl(var(--green-900), <alpha-value>)',
+			},
+			purple: {
+				100: 'hsl(var(--purple-100), <alpha-value>)',
+				200: 'hsl(var(--purple-200), <alpha-value>)',
+				600: 'hsl(var(--purple-500), <alpha-value>)',
+				700: 'hsl(var(--purple-700), <alpha-value>)',
+				800: 'hsl(var(--purple-800), <alpha-value>)',
+				900: 'hsl(var(--purple-900), <alpha-value>)',
+			},
+			red: {
+				100: 'hsl(var(--red-100), <alpha-value>)',
+				200: 'hsl(var(--red-200), <alpha-value>)',
+				600: 'hsl(var(--red-500), <alpha-value>)',
+				700: 'hsl(var(--red-700), <alpha-value>)',
+				800: 'hsl(var(--red-800), <alpha-value>)',
+				900: 'hsl(var(--red-900), <alpha-value>)',
+			},
+			yellow: {
+				100: 'hsl(var(--yellow-100), <alpha-value>)',
+				200: 'hsl(var(--yellow-200), <alpha-value>)',
+				600: 'hsl(var(--yellow-500), <alpha-value>)',
+				700: 'hsl(var(--yellow-700), <alpha-value>)',
+				800: 'hsl(var(--yellow-800), <alpha-value>)',
+				900: 'hsl(var(--yellow-900), <alpha-value>)',
+			},
 		},
 		extend: {},
 	},
